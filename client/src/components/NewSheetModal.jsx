66f517f9bd8c0f63cb7edfb8267278f767889981
mattkellyirl <matthew.kelly@digicom.net.au>
2024-05-29 21:08:56 +0930

    added height input fields for new sheet modal

--- client/src/components/NewSheetModal.jsx
+++ client/src/components/NewSheetModal.jsx
@@ -9,7 +9,7 @@ function NewSheetModal({ isOpen, onClose }) {
       aria-hidden="false"
       className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto bg-gray-900 bg-opacity-50"
     >
-      <div className="relative w-full max-w-md max-h-full p-4">
+      <div className="relative w-full max-w-screen-xl max-h-full p-4">
         <div className="relative bg-white rounded-lg shadow">
           <div className="flex items-center justify-between p-4 border-b rounded-t md:p-5">
             <h3 className="text-lg font-semibold text-black">
@@ -39,96 +39,172 @@ function NewSheetModal({ isOpen, onClose }) {
             </button>
           </div>
           <form className="p-4 md:p-5">
-            <div className="grid grid-cols-2 gap-4 mb-4">
-              <div className="col-span-2">
-                <label
-                  htmlFor="car"
-                  className="block mb-2 text-sm font-semibold text-black"
-                >
-                  Car
-                </label>
-                <select
-                  name="car"
-                  id="car"
-                  className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
-                  required
-                >
-                  <option value="" disabled selected hidden>
-                    Please select...
-                  </option>
-                  <option value="Car 1">Car 1</option>
-                  <option value="Car 2">Car 2</option>
-                  <option value="Car 3">Car 3</option>
-                </select>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+              <div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="event"
+                    className="block mb-2 text-sm font-semibold text-black"
+                  >
+                    Event
+                  </label>
+                  <select
+                    name="event"
+                    id="event"
+                    className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
+                    required
+                  >
+                    <option value="" disabled selected hidden>
+                      Please select...
+                    </option>
+                    <option value="Event 1">Event 1</option>
+                    <option value="Event 2">Event 2</option>
+                    <option value="Event 3">Event 3</option>
+                  </select>
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="car"
+                    className="block mb-2 text-sm font-semibold text-black"
+                  >
+                    Car
+                  </label>
+                  <select
+                    name="car"
+                    id="car"
+                    className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
+                    required
+                  >
+                    <option value="" disabled selected hidden>
+                      Please select...
+                    </option>
+                    <option value="Car 1">Car 1</option>
+                    <option value="Car 2">Car 2</option>
+                    <option value="Car 3">Car 3</option>
+                  </select>
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="session"
+                    className="block mb-2 text-sm font-semibold text-black"
+                  >
+                    Session
+                  </label>
+                  <select
+                    name="session"
+                    id="session"
+                    className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
+                    required
+                  >
+                    <option value="" disabled selected hidden>
+                      Please select...
+                    </option>
+                    <option value="Practice 1">Practice 1</option>
+                    <option value="Practice 2">Practice 2</option>
+                    <option value="Practice 3">Practice 3</option>
+                    <option value="Practice 4">Practice 4</option>
+                    <option value="Practice 5">Practice 5</option>
+                    <option value="Qualifying">Qualifying</option>
+                    <option value="Race 1">Race 1</option>
+                    <option value="Race 2">Race 2</option>
+                    <option value="Race 3">Race 3</option>
+                    <option value="Race 4">Race 4</option>
+                    <option value="Race 5">Race 5</option>
+                  </select>
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="driver"
+                    className="block mb-2 text-sm font-semibold text-black"
+                  >
+                    Driver
+                  </label>
+                  <input
+                    type="text"
+                    name="driver"
+                    id="driver"
+                    className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
+                    placeholder="Enter driver name"
+                    required
+                  />
+                </div>
               </div>
 
-              <div className="col-span-2">
-                <label
-                  htmlFor="event"
-                  className="block mb-2 text-sm font-semibold text-black"
-                >
-                  Event
-                </label>
-                <select
-                  name="event"
-                  id="event"
-                  className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
-                  required
-                >
-                  <option value="" disabled selected hidden>
-                    Please select...
-                  </option>
-                  <option value="Event 1">Event 1</option>
-                  <option value="Event 2">Event 2</option>
-                  <option value="Event 3">Event 3</option>
-                </select>
-              </div>
+              <div>
+                <div className="flex mb-4 gap-4">
+                  <div className="w-1/2">
+                    <label
+                      htmlFor="leftfront"
+                      className="block mb-2 text-sm font-semibold text-black"
+                    >
+                      Left Front
+                    </label>
+                    <input
+                      type="number"
+                      name="leftfront"
+                      id="leftfront"
+                      className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
+                      placeholder="Enter ride height"
+                      required
+                    />
+                  </div>
 
-              <div className="col-span-2">
-                <label
-                  htmlFor="session"
-                  className="block mb-2 text-sm font-semibold text-black"
-                >
-                  Session
-                </label>
-                <select
-                  name="session"
-                  id="session"
-                  className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
-                  required
-                >
-                  <option value="" disabled selected hidden>
-                    Please select...
-                  </option>
-                  <option value="Practice 1">Practice 1</option>
-                  <option value="Practice 2">Practice 2</option>
-                  <option value="Practice 3">Practice 3</option>
-                  <option value="Practice 4">Practice 4</option>
-                  <option value="Practice 5">Practice 5</option>
-                  <option value="Qualifying">Qualifying</option>
-                  <option value="Race 1">Race 1</option>
-                  <option value="Race 2">Race 2</option>
-                  <option value="Race 3">Race 3</option>
-                  <option value="Race 4">Race 4</option>
-                  <option value="Race 5">Race 5</option>
-                </select>
-              </div>
+                  <div className="w-1/2">
+                    <label
+                      htmlFor="rightfront"
+                      className="block mb-2 text-sm font-semibold text-black"
+                    >
+                      Right Front
+                    </label>
+                    <input
+                      type="number"
+                      name="rightfront"
+                      id="rightfront"
+                      className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
+                      placeholder="Enter ride height"
+                      required
+                    />
+                  </div>
+                </div>
 
-              <div className="col-span-2">
-                <label
-                  htmlFor="driver"
-                  className="block mb-2 text-sm font-semibold text-black"
-                >
-                  Driver
-                </label>
-                <input
-                  type="text"
-                  name="driver"
-                  id="driver"
-                  className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
-                  placeholder="Enter driver name"
-                  required
-                />
+                <div className="flex mb-4 gap-4">
+                  <div className="w-1/2">
+                    <label
+                      htmlFor="leftrear"
+                      className="block mb-2 text-sm font-semibold text-black"
+                    >
+                      Left Rear
+                    </label>
+                    <input
+                      type="number"
+                      name="leftrear"
+                      id="leftrear"
+                      className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
+                      placeholder="Enter ride height"
+                      required
+                    />
+                  </div>
+
+                  <div className="w-1/2">
+                    <label
+                      htmlFor="rightrear"
+                      className="block mb-2 text-sm font-semibold text-black"
+                    >
+                      Right Rear
+                    </label>
+                    <input
+                      type="number"
+                      name="rightrear"
+                      id="rightrear"
+                      className="block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600"
+                      placeholder="Enter ride height"
+                      required
+                    />
+                  </div>
+                </div>
               </div>
             </div>
             <div className="flex items-center justify-center">

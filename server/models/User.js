567397aa72455cd2382fe223160d0ab220b50f62
mattkellyirl <matthew.kelly@digicom.net.au>
2024-05-30 19:46:23 +0930

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+});
+
+// Hashes password before saving into database, only if password is new or has been modified
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+// Compare incoming password with hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

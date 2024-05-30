2181104d68ac5a42c07f44ce7afa5c17f6f1d55b
mattkellyirl <matthew.kelly@digicom.net.au>
2024-05-30 13:56:36 +0930

    updated button colour, disabled field colours and padding/margin for new event modal

--- client/src/components/NewEventModal.jsx
+++ client/src/components/NewEventModal.jsx
@@ -42,8 +42,8 @@ function NewEventModal({ isOpen, onClose }) {
               <span className="sr-only">Close modal</span>
             </button>
           </div>
-          <form className="p-4 md:p-5">
-            <div className="grid grid-cols-2 gap-4 mb-4">
+          <form className="p-4 md:p-6">
+            <div className="grid grid-cols-2 gap-4 mb-6">
               <div className="col-span-2">
                 <label
                   htmlFor="type"
@@ -70,7 +70,7 @@ function NewEventModal({ isOpen, onClose }) {
                 <label
                   htmlFor="championship"
                   className={`block mb-2 text-sm font-semibold ${
-                    !isRaceEventSelected ? "text-gray-500" : "text-black"
+                    !isRaceEventSelected ? "text-gray-400" : "text-black"
                   }`}
                 >
                   Championship
@@ -79,7 +79,7 @@ function NewEventModal({ isOpen, onClose }) {
                   name="championship"
                   id="championship"
                   className={`block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600 ${
-                    !isRaceEventSelected ? "text-gray-500" : ""
+                    !isRaceEventSelected ? "text-gray-400" : ""
                   }`}
                   defaultValue="N/A"
                   required
@@ -105,7 +105,7 @@ function NewEventModal({ isOpen, onClose }) {
                 <label
                   htmlFor="championshipRound"
                   className={`block mb-2 text-sm font-semibold ${
-                    !isRaceEventSelected ? "text-gray-500" : "text-black"
+                    !isRaceEventSelected ? "text-gray-400" : "text-black"
                   }`}
                 >
                   Round
@@ -115,7 +115,7 @@ function NewEventModal({ isOpen, onClose }) {
                   name="championshipRound"
                   id="championshipRound"
                   className={`block w-full p-2.5 text-sm text-black bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600 ${
-                    !isRaceEventSelected ? "text-gray-500" : ""
+                    !isRaceEventSelected ? "text-gray-400" : ""
                   }`}
                   placeholder="Enter championship round"
                   defaultValue={1}
@@ -160,7 +160,7 @@ function NewEventModal({ isOpen, onClose }) {
             <div className="flex items-center justify-center">
               <button
                 type="submit"
-                className="px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                className="px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
               >
                 Add Event
               </button>

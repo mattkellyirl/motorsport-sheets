546c104c53490bcc2f27b66af5d5411a2108e44c
mattkellyirl <matthew.kelly@digicom.net.au>
2024-05-30 19:26:44 +0930

    updated resolvers.js with functioning login and add user functions

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { User } = require("../models");
-// const { signToken, AuthenticationError } = require("../utils/auth"); don't remove this line, will need later
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -11,7 +11,7 @@ const resolvers = {
         throw new Error("Error fetching users");
       }
     },
-
+    //  Fetch user by ID
     user: async (_, { id }) => {
       try {
         return User.findById(id);
@@ -21,7 +21,53 @@ const resolvers = {
     },
   },
 
-  Mutation: {},
+  Mutation: {
+    addUser: async (_, { email, password }) => {
+      try {
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+
+        // If user exists, throw error
+        if (existingUser) {
+          throw new Error("User already exists");
+        } else {
+          // If user doesn't exist, create user and sign token
+          const newUser = await User.create({ email, password });
+          const token = signToken(newUser);
+
+          // Return token and new user
+          return { token, user: newUser };
+        }
+      } catch (error) {
+        throw new Error("Error adding user");
+      }
+    },
+
+    loginUser: async (_, { email, password }) => {
+      try {
+        // Check to see if user exists
+        const user = await User.findOne({ email });
+
+        // If user doesn't exist, throw error
+        if (!user) {
+          throw new AuthenticationError("Invalid credentials");
+        } else {
+          // If user exists, check password
+          const validPassword = await user.isCorrectPassword(password);
+          // If password is incorrect, throw error
+          if (!validPassword) {
+            throw new AuthenticationError("Invalid credentials");
+          } else {
+            // If password is correct, sign in user and return token and user
+            const token = signToken(user);
+            return { token, user };
+          }
+        }
+      } catch (error) {
+        throw new Error("Error logging in user");
+      }
+    },
+  },
 };
 
 module.exports = resolvers;

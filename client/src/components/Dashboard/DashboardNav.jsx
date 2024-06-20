fb16bbf2262367e939093dbb81875f931873bc9c
mattkellyirl <matthew.kelly@digicom.net.au>
2024-06-20 23:47:37 +0930

    updated DashboardNav.jsx component with dropdown menu for user account

--- client/src/components/Dashboard/DashboardNav.jsx
+++ client/src/components/Dashboard/DashboardNav.jsx
@@ -1,21 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthService from "../../utils/authService";
-import { Link, useNavigate } from "react-router-dom";
-
-import { RiArrowDropDownLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 import {
   BiSolidSpreadsheet,
   BiSpreadsheet,
-  BiSolidCar,
   BiSolidTimer,
 } from "react-icons/bi";
 import {
   MdSpaceDashboard,
   MdEventNote,
-  MdLogout,
   MdOutlineTimer,
   MdSportsMotorsports,
+  MdOutlineKeyboardArrowDown,
 } from "react-icons/md";
 import { FaFlagCheckered, FaCarSide } from "react-icons/fa6";
 import { TbListCheck } from "react-icons/tb";
@@ -23,6 +20,9 @@ import { TbListCheck } from "react-icons/tb";
 import logo from "../../../assets/motorsport-sheets.png";
 
 function DashboardNav() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const handleUserLogout = () => {
     try {
       if (!AuthService.loggedIn()) {
@@ -36,6 +36,14 @@ function DashboardNav() {
     }
   };
 
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div>
       <button
@@ -44,6 +52,7 @@ function DashboardNav() {
         aria-controls="logo-sidebar"
         type="button"
         className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+        onClick={toggleNavbar}
       >
         <span className="sr-only">Open sidebar</span>
         <svg
@@ -66,33 +75,65 @@ function DashboardNav() {
         className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100">
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100 border-r border-gray-200">
           <Link to="/dashboard">
-            <div className="flex justify-center pl-2.5 mb-4">
+            <div className="flex justify-center">
               <img src={logo} alt="Motorsport Sheets" className="h-12" />
             </div>
           </Link>
 
           <ul className="mt-4 space-y-2 font-medium">
-            {/* User Profile */}
-            <button className="pl-2 py-2 rounded-lg flex items-center justify-between w-full hover:bg-gray-200 group">
+            {/* User Account */}
+            <button
+              className="pl-2 pr-1 py-2 rounded-lg flex items-center justify-between w-full group hover:bg-gray-200"
+              onClick={toggleDropdown}
+            >
               <div className="flex flex-row items-center">
                 <img
-                  class="w-8 h-8 mr-2 rounded-full"
+                  className="w-6 h-6 mr-2 rounded-full"
                   src="https://media.licdn.com/dms/image/D5603AQEYAfZt7WFN0Q/profile-displayphoto-shrink_800_800/0/1696734857420?e=1724284800&v=beta&t=l1xfuvNbIZfPvXx2To3DHBpifYtmlZ_UInaAt-sWyj4"
                   alt="User Profile Picture"
                 ></img>
 
                 <div className="flex flex-col items-start">
-                  <p className="text-black font-medium text-sm">Matt Kelly</p>
-                  <p className="text-gray-500 text-xs">
+                  <span className="text-black font-medium text-xs">
+                    Matt Kelly
+                  </span>
+                  <span className="text-gray-500 font-medium text-xs tracking-tight">
                     matt@autosportindex.com
-                  </p>
+                  </span>
                 </div>
               </div>
 
-              <RiArrowDropDownLine className="w-8 h-8" />
+              <MdOutlineKeyboardArrowDown className="w-6 h-6 text-black" />
             </button>
+
+            {/* User Account Dropdown Menu */}
+            {isDropdownOpen && (
+              <ul className="space-y-2 font-medium">
+                <li>
+                  <Link
+                    to="/profile"
+                    className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
+                  >
+                    <div className="flex flex-row items-center">
+                      <span className="ml-8">Settings</span>
+                    </div>
+                  </Link>
+                </li>
+
+                <li>
+                  <button
+                    onClick={handleUserLogout}
+                    className="flex items-center p-2 text-red-600 rounded-lg w-full hover:bg-red-600 hover:text-white group"
+                  >
+                    <div className="flex flex-row items-center">
+                      <span className="ml-8">Logout</span>
+                    </div>
+                  </button>
+                </li>
+              </ul>
+            )}
 
             {/* Dashboard */}
             <li>
@@ -100,12 +141,12 @@ function DashboardNav() {
                 to="/dashboard"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <MdSpaceDashboard className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <MdSpaceDashboard className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Dashboard</span>
               </Link>
             </li>
 
-            <hr></hr>
+            <hr className="border-gray-200"></hr>
 
             {/* Driver, Car and Event Setup */}
             <li>
@@ -113,7 +154,7 @@ function DashboardNav() {
                 to="/dashboard/drivers"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <MdSportsMotorsports className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <MdSportsMotorsports className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Drivers</span>
               </Link>
             </li>
@@ -122,7 +163,7 @@ function DashboardNav() {
                 to="/dashboard/cars"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <FaCarSide className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <FaCarSide className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Cars</span>
               </Link>
             </li>
@@ -131,12 +172,12 @@ function DashboardNav() {
                 to="/dashboard/events"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <MdEventNote className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <MdEventNote className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Events</span>
               </Link>
             </li>
 
-            <hr></hr>
+            <hr className="border-gray-200"></hr>
 
             {/* Sheets */}
             <li>
@@ -144,7 +185,7 @@ function DashboardNav() {
                 to="/dashboard/run-sheets"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <BiSpreadsheet className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <BiSpreadsheet className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Run Sheets</span>
               </Link>
             </li>
@@ -153,7 +194,7 @@ function DashboardNav() {
                 to="/dashboard/sheets"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <BiSolidSpreadsheet className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <BiSolidSpreadsheet className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Setup Sheets</span>
               </Link>
             </li>
@@ -162,7 +203,7 @@ function DashboardNav() {
                 to="/dashboard/check-sheets"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <TbListCheck className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <TbListCheck className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Check Sheets</span>
               </Link>
             </li>
@@ -171,12 +212,12 @@ function DashboardNav() {
                 to="/dashboard/lap-sheets"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <MdOutlineTimer className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <MdOutlineTimer className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Lap Sheets</span>
               </Link>
             </li>
 
-            <hr></hr>
+            <hr className="border-gray-200"></hr>
 
             {/* Driver Tools */}
             <li>
@@ -184,7 +225,7 @@ function DashboardNav() {
                 to="/dashboard/maps"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <FaFlagCheckered className="w-5 h-4 text-gray-500 transition duration-75 group-hover:text-black" />
+                <FaFlagCheckered className="w-5 h-4 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Track Maps</span>
                 <span className="ml-auto px-2 text-sm font-medium text-blue-800 bg-blue-100 rounded-full">
                   Pro
@@ -196,25 +237,12 @@ function DashboardNav() {
                 to="/dashboard/reaction-test"
                 className="flex items-center p-2 text-black rounded-lg hover:bg-gray-200 group"
               >
-                <BiSolidTimer className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
+                <BiSolidTimer className="w-5 h-5 text-gray-500 group-hover:text-black" />
                 <span className="ml-3">Reaction Test</span>
                 <span className="ml-auto px-2 text-sm font-medium text-blue-800 bg-blue-100 rounded-full">
                   Pro
                 </span>
               </Link>
-            </li>
-
-            <hr></hr>
-
-            {/* Logout */}
-            <li>
-              <button
-                onClick={handleUserLogout}
-                className="flex items-center p-2 w-full text-black rounded-lg hover:bg-gray-200 group"
-              >
-                <MdLogout className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-black" />
-                <span className="ml-3">Logout</span>
-              </button>
             </li>
           </ul>
         </div>
